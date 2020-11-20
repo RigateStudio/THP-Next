09ae195e7ed7659f8cd457a2f290e0b7011f089e
Paul Maillardet <paul@pulsanova.com>
2020-11-20 13:38:05 +0100

    Ajoute l'affichage du scroll dans la page de listing

--- custom-hooks/src/index.jsx
+++ custom-hooks/src/index.jsx
@@ -6,15 +6,11 @@ import Clients from './pages/Clients';
 
 faker.locale = 'fr'
 
-class App extends React.Component {
-    render() {
-        return (
-            <main className="App">
-                <h1>Liste des clients</h1>
-                <Clients />
-            </main>
-        );
-    }
-}
+const App = () => (
+    <main className="App">
+        <h1>Liste des clients</h1>
+        <Clients />
+    </main>
+);
 
 ReactDOM.render(<App />, document.querySelector('#root'));
--- custom-hooks/src/pages/Clients/Listing/index.jsx
+++ custom-hooks/src/pages/Clients/Listing/index.jsx
@@ -1,17 +1,39 @@
 import './index.scss';
+import { useLayoutEffect, useState } from 'react';
 import Client from 'components/Client';
 
-const ClientsListing = ({ data }) => (
-    <ul className="ClientsListing">
-        {data.map(({ id, firstName, lastName, phoneNumber }) => (
-            <Client
-                key={id}
-                firstName={firstName}
-                lastName={lastName}
-                phoneNumber={phoneNumber}
-            />
-        ))}
-    </ul>
-);
+const ClientsListing = ({ data }) => {
+    const [scrollY, setScrollY] = useState(0);
+
+    useLayoutEffect(() => {
+        const handleScroll = () => {
+            const scrollY = document.documentElement.scrollTop;
+            setScrollY(scrollY);
+        };
+
+        window.addEventListener('scroll', handleScroll);
+        return () => {
+            window.removeEventListener('scroll', handleScroll)
+        }
+    }, []);
+
+    return (
+        <div className="ClientsListing">
+            <h4 className="ClientsListing__scroll-pos">
+                Scroll: {Math.round(scrollY)}px
+            </h4>
+            <ul className="ClientsListing__list">
+                {data.map(({ id, firstName, lastName, phoneNumber }) => (
+                    <Client
+                        key={id}
+                        firstName={firstName}
+                        lastName={lastName}
+                        phoneNumber={phoneNumber}
+                    />
+                ))}
+            </ul>
+        </div>
+    );
+};
 
 export default ClientsListing;
